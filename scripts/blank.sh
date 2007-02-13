ef82d8f912fa6e407b8f76a299514ad37dc61c8a
Willy Tarreau <w@1wt.eu>
2007-02-13 22:24:44 +0100

    * mksp-sqfs : add the '-ln' argument to create symlinks

--- scripts/blank.sh
+++ scripts/blank.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #/opt/schily/bin/cdrecord dev=0,0,0 speed=8 blank=fast -v
-cdrecord dev=ATAPI:0,1,0 speed=8 blank=fast -v
+# en ATAPI :
+#cdrecord dev=ATAPI:0,1,0 speed=8 blank=fast -v
+# en IDE-SCSI :
+cdrecord dev=0,0,0 speed=8 blank=fast -v
 
